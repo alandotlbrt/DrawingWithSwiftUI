1cb2bfe42801b23f91bd682d5ef590283864edab
alandotlbrt <alanlebrument@icloud.com>
2024-08-30 11:05:54 +0200

    Initial Commit

---
+++ DrawingWithSwiftUI/DrawingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingWithSwiftUIApp.swift
+//  DrawingWithSwiftUI
+//
+//  Created by Alan Lebrument on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
